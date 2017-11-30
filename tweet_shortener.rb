3d116f92291b090a985f4ac6295a70aa39383395
Kelly Lai <zhiyinl5633@hstat.org>
2017-11-30 01:14:06 +0000

    Done

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,5 +39,9 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-    tweet[0...140] 
+    if word_substituter(tweet).length > 140
+        word_substituter(tweet)[0..136] + "..."
+    else
+        tweet
+    end
 end
