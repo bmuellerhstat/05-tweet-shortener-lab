787acd7e71578fac3733ceba19b29758409428d6
Kellyx1636 <kellyx1636@hstat.org>
2017-12-01 13:39:52 +0000

    Made the dictionary hash inside the dictionary method.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+# Write your code here. 
+#GitHub Link: https://github.com/kellyx1636/05-string-shortener-lab  
+
+def dictionary()   #Hash is put into a method so that it can be called? 
+    dictionary = {
+    "hello" => "hi", 
+    "to" => "2", 
+    "two" => "2", 
+    "too" => "2", 
+    "for" => "4", 
+    "four" => "4", 
+    "be" => "b", 
+    "you" => "u", 
+    "at" => "@", 
+    "and" => "&"
+}
+end
+
+
+ 
