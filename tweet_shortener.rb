40456710c7a4d4cc533a083bbbfddd0f2aaab4dc
Jeffrey Guan <jeffreyg2240@hstat.org>
2018-11-26 23:02:09 +0000

    Finished HW #12: 05-tweet-shortener-lab

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def word_substituter(tweet)
+    tweet.gsub((/be\b/), "b").gsub(/hello/, "hi").gsub(/((too|to)|two)/,"2").gsub(/((for|(four)|(For|Four)))/,"4").gsub(/you\b/, "u").gsub(/and\b/, "&").gsub(/\bat\b/, "@")
+
+end
+def bulk_tweet_shortener(tweets)
+tweets.each{|tweets|
+puts word_substituter(tweets)
+}
+end
+def selective_tweet_shortener(tweet)
+    if(tweet.length>140)
+       word_substituter(tweet) 
+    else
+        tweet
+    end
+end
+def shortened_tweet_truncator(tweet)
+    if(tweet.length>140)
+       word_substituter(tweet)[0...140]
+    else
+        tweet
+    end
+end
