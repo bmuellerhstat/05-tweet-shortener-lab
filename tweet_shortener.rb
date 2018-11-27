7ac430c100b64a5d91cb76aca6ea8e63faaaf422
Kasper Gacek <kasperg3971@hstat.org>
2018-11-27 03:02:36 +0000

    Finished Tweet_Shortener

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,44 @@
 # Write your code here.
+def dictionary
+    {
+    "hello" => 'hi',
+"to" => '2' ,
+"two" => '2',
+"too" => '2',
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@" ,
+"and" => "&"
+}
+end
+def word_substituter(string)
+    string.split.collect do |tweet|
+        if dictionary.keys.include?(tweet.downcase)
+            tweet = dictionary[tweet.downcase]
+        else
+            tweet
+        end
+    end.join(" ")
+        
+end
+def bulk_tweet_shortener(tweets)
+    tweets.each do |tweet|
+        puts word_substituter(tweet)
+    end
+end
+def selective_tweet_shortener(tweets)
+    if tweets.length > 140
+        word_substituter(tweets)
+    else 
+        tweets
+    end
+end
+def shortened_tweet_truncator(tweet)
+    if word_substituter(tweet).length > 140
+        word_substituter(tweet)[0..136] << "..."
+else 
+    tweet
+end
+end
