fe36ee94cf0833d37d4640ad39520c2ba436f4f3
Eva Noertoft <evan2304@hstat.org>
2018-11-27 10:50:49 +0000

    attempt

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,41 @@
 # Write your code here.
+
+def dictionary 
+    { 
+    "hello" => 'hi',
+    "to"=>  '2',
+    "two"=>  '2',
+    "too" =>  '2',
+    "for, four" =>  '4',
+    'be'=> 'b',
+    'you'=>  'u',
+    "at" =>  "@",
+    "and"=>  "&"
+    }
+end 
+
+def word_substituter(long_tweet)
+    long_words = long_tweet.split(" ")
+    substitions = dictionary
+    long_words.each |word|
+    if word == substitions.keys
+        long_tweet.sub! word, "substitions.value"
+    end
+    
+end 
+
+def bulk_tweet_shortener(tweet)
+    puts word_substituter
+end
+
+def selective_tweet_shortener(tweet)
+    if tweet([a-z]).count > 140
+        puts bulk_tweet_shortener
+    elsif tweet([a-z]).count < 130
+        tweet
+    else
+        tweet
+    end
+end 
+
+
