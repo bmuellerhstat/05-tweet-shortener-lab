ad32e2530596398f72f71d7dc5916f36ff21abc7
Toby lau <yatchol6214@hstat.org>
2018-11-27 09:49:01 +0000

    homework

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,48 @@
 # Write your code here.
+def word_substituter(tweet)
+    words = tweet.split(" ")
+    
+    words.map do |word|
+        case word.downcase
+        when "hello"
+            "hi"
+        when "to", "two", "too"
+             "2"
+        when "for", "four"
+             "4"
+        when "be"
+             "b"
+        when "you"
+             "u"
+        when "at"
+             "@"
+        when "and"
+             "&"
+        else 
+             word
+        end
+    end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+    tweets.each do |tweet|
+        tweet = word_substituter(tweet)
+        puts "#{tweet}"
+    end
+end
+
+def selective_tweet_shortener(tweet)
+    if tweet.length > 140
+        word_substituter(tweet)
+    else
+        tweet
+    end
+end
+
+def shortened_tweet_truncator(tweet)
+    if tweet.length > 140
+    tweet[1..137]+"..."
+    else
+        tweet
+    end
+end    
